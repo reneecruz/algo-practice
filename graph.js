0074ccae8d4f7e6d0e664186f22038cb50d005ef
Renée Cruz <reneebasscruz@gmail.com>
2020-03-01 22:39:51 -0500

    add edge

--- graph.js
+++ graph.js
@@ -6,4 +6,9 @@ class Graph {
     addVertex(vertex) {
         if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
       }
+
+    addEdge(v1, v2) {
+        this.adjacencyList[v1].push(v2)
+        this.adjacencyList[v2].push(v1)
+    }
 }
