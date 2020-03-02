ac31b2559c0218f4aa30de0e9412e491aaafb50e
Renée Cruz <reneebasscruz@gmail.com>
2020-03-01 22:42:43 -0500

    remove vertex

--- graph.js
+++ graph.js
@@ -16,4 +16,12 @@ class Graph {
         this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2)
         this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1) 
     }
+
+    removeVertex(vtx) {
+        this.adjacencyList[vtx].forEach(v => {
+          this.removeEdge(vtx, v);
+        })
+        delete this.adjacencyList[vtx]
+      }
+    
 }
