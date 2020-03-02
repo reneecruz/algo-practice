b2c662389e549e87423ed054f82a324aa3b5ac97
Renée Cruz <reneebasscruz@gmail.com>
2020-03-01 22:41:53 -0500

    remove edge

--- graph.js
+++ graph.js
@@ -11,4 +11,9 @@ class Graph {
         this.adjacencyList[v1].push(v2)
         this.adjacencyList[v2].push(v1)
     }
+
+    removeEdge(v1, v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2)
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1) 
+    }
 }
